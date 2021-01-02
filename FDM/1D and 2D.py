2c83db9d9e7418760488bd0b56ddf297573f121b
Amin <amin.nadimy19@imperial.ac.uk>
2021-01-02 16:28:34 +0000

    DFM

---
+++ FDM/1D and 2D.py
@@ -0,0 +1,149 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#------------------------------- 2-D FDM------------------------------------------
+C= .01                           # CLF number
+nt = 5000                        # Number of time steps
+nx = 100                         # Number of steps in x-direction
+ny = 200                         # Number of steps in y-direction
+L = 2                            # Lengths of the domain in x and y directions
+dx = L/(nx-1)                    # dispacement legth at each time-step in x-direction
+dy = L/(ny-1)                    # dispacement legth at each time-step in y-direction
+d_displacement = (dx**2+dy**2)**0.5 # overall displacement of the wave in both directions
+x=np.linspace(0, 2, nx)
+y=np.linspace(0,2,ny)
+c_x = 0.1                        # Wave velocity in x-dir
+c_y = 0.1                        # Wave velocity in y-di
+c = (c_x**2+c_y**2)**0.5            # overall velocity of the wave
+dt = C*d_displacement/c          # length of time step. 
+U = np.zeros((ny,nx))            # Wave matrix
+Un = np.zeros((ny,nx))           # Dummy variable to save current components of U
+U1=U2=U3 = U                     # Dummy matrices to plot 3 time steps
+
+U[int(ny*.1):int(ny*.3), int(nx*0.1):int(nx*0.3)]=1     # Defining wave components
+X, Y =np.meshgrid(x,y)           # Creating a mesh grid
+
+#--------------------------------- Initial Conds ------------------------------
+U[:, 0]= U[:, -1] = 0
+U[0,:] = U[-1,:] = 0
+
+#------------------------------------------------------------------------------
+for n in range(nt+1):             # marching in time steps
+    Un=U.copy()                   # dummy vbl to save current solutions
+    U[1:,1:] = Un[1:,1:] - c_y*(dt/dy)*(Un[1:,1:] - Un[:-1,1:]) - c_x*(dt/dx)*(Un[1:,1:] - Un[1:, :-1])
+
+    if n==0:                    # saving the first time step to plot
+        U1 = Un
+    elif n==int(nt/2):          # saving the half time step to plot
+        U2 = Un
+    elif n==int(nt*0.99):       # saving the final time step to plot
+        U3 = Un
+
+#------------------------------ Plotting the results ----------------------------        
+plt.figure()        
+ax = plt.gca(projection='3d')
+ax.plot_surface(X, Y, U1 , label='t=0')
+ax.plot_surface(X, Y, U2, label='t=nt/2')
+ax.plot_surface(X, Y, U3, label='t=final')
+ax.set_ylabel('$y$')
+ax.set_xlabel('$x$')
+ax.set_zlabel('$U$')
+plt.legend()
+plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, U[:], cmap=plt.cm.viridis)
+#ax.plot(x, y, U, label='Square Wave')
+plt.show()
+
+######################################## 1-D  #################################
+#C = .6
+#dt = 1
+#dx = 1
+#nx = 100
+#nt = 200
+#
+#u = np.zeros(nx)
+#u[int(nx*0.05):int(nx*0.2)]=1
+#u[0] = u[nx-1] = 0
+#
+#
+#for n in range(nt):
+#    plt.clf()
+#    un = u.copy()
+#    for i in range(nx):
+#        u[i] = un[i] - C*dt/dx*(un[i]-un[i-1])
+#    plt.axis([0,1, 0,1])
+#    plt.plot(np.linspace(0,2,nx),u)
+#    plt.pause(0.0001)
+
+#================================ Sin (x) =====================================       
+
+# C  = 1
+# c= .1
+# L=2
+# nx = 500                    # distance which the wave travels
+# nt = 40                     # time length
+# dx = L/(nx-1)
+# dt = .04#C*dx/c
+
+
+# u = np.zeros(nx)
+# u[5:30]=1                 # It's just an example IC which I chose
+# x  = np.arange(0, nx)*dx#x=np.linspace(0,20,nx)
+# plt.plot(x,u)
+
+# #Initial Conditions:
+
+# u[nx-1]= 0
+
+# for n in range(nt):
+#     u[0] = np.cos(n*dt)
+#     plt.clf()
+#     un = u.copy()
+#     for i in range(1, nx-1):
+#          # Forward euler's method for time and backward for space
+#         u[i] = un[i] - c*(dt/dx)*(un[i]-un[i-1])
+
+#     plt.axis([0,1, 0,1.2])
+#     plt.plot(x,u)
+#     plt.pause(.0001)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
